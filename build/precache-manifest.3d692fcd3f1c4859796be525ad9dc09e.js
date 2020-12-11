fd67c2583a27b6e1dc3c49993ae46830e59c53ac
amandeep2251 <47323384+amandeep2251@users.noreply.github.com>
2020-12-11 15:55:32 +0530

    Yes
    Yes

---
+++ build/precache-manifest.3d692fcd3f1c4859796be525ad9dc09e.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65243f450f8fd3d6cd3d858dbdc6b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f037c6337146d80ca519",
+    "url": "/static/css/2.614afd85.chunk.css"
+  },
+  {
+    "revision": "de50b9909871883c33e6",
+    "url": "/static/css/main.cfe7ae56.chunk.css"
+  },
+  {
+    "revision": "f037c6337146d80ca519",
+    "url": "/static/js/2.ee855203.chunk.js"
+  },
+  {
+    "revision": "de50b9909871883c33e6",
+    "url": "/static/js/main.70ec3949.chunk.js"
+  },
+  {
+    "revision": "88dd8927fae8627a17ad",
+    "url": "/static/js/runtime-main.5c20a2a3.js"
+  },
+  {
+    "revision": "1ce62b82b61eee3426384bda4b188ada",
+    "url": "/static/media/AddToBookList.1ce62b82.png"
+  },
+  {
+    "revision": "bd5e676fbacf93068f2825a973541ba8",
+    "url": "/static/media/BookWhite.bd5e676f.png"
+  },
+  {
+    "revision": "8a7118984c3e98ef7dbebf3745fe5272",
+    "url": "/static/media/CreateBookListIK.8a711898.png"
+  },
+  {
+    "revision": "b047f6aed25a5c4f18e455033f3c1f5d",
+    "url": "/static/media/HTML.b047f6ae.png"
+  },
+  {
+    "revision": "831187a48da0edfbe5300a254e552d3c",
+    "url": "/static/media/Mari.831187a4.pdf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "55b7b1d414ea6d2094b85e3a834e32b8",
+    "url": "/static/media/MontserratAlternates-Medium.55b7b1d4.ttf"
+  },
+  {
+    "revision": "778e16de3b7bbf4100f31ff4d6307216",
+    "url": "/static/media/MontserratAlternates-Regular.778e16de.ttf"
+  },
+  {
+    "revision": "d182a4e3cece6a15375867f6efea9d9e",
+    "url": "/static/media/Mulish-Bold.d182a4e3.ttf"
+  },
+  {
+    "revision": "9105ec56b65711a43a9bf58e2c2dd84a",
+    "url": "/static/media/Mulish-ExtraLight.9105ec56.ttf"
+  },
+  {
+    "revision": "9bc1b362902a1c3f5017caa59b0baa9a",
+    "url": "/static/media/Mulish-Light.9bc1b362.ttf"
+  },
+  {
+    "revision": "389e4d23bccb213ca224d555fb69fb3e",
+    "url": "/static/media/Mulish-Medium.389e4d23.ttf"
+  },
+  {
+    "revision": "e128ac44faa84b2d59c10c016fad0778",
+    "url": "/static/media/Mulish-Regular.e128ac44.ttf"
+  },
+  {
+    "revision": "4728adb41b90be2262231a63fa5ec162",
+    "url": "/static/media/MyBooklists.4728adb4.png"
+  },
+  {
+    "revision": "72edb6958007de20886789f57c25f901",
+    "url": "/static/media/MyBooks.72edb695.png"
+  },
+  {
+    "revision": "6956751778259011e977de168c0f740b",
+    "url": "/static/media/PDF.69567517.png"
+  },
+  {
+    "revision": "f6ea751e936ade6edcd03a26b8153b4a",
+    "url": "/static/media/Poppins-Light.f6ea751e.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "7802d8b27fcb19893ce6b38c0789268e",
+    "url": "/static/media/Raleway-Bold.7802d8b2.ttf"
+  },
+  {
+    "revision": "c9503ab0f939e9d37fcfb59b25acf8b3",
+    "url": "/static/media/Raleway-ExtraBold.c9503ab0.ttf"
+  },
+  {
+    "revision": "8a192102b50118c45033e53ce897f103",
+    "url": "/static/media/Raleway-SemiBold.8a192102.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "f965963f6d8b2116bff9b297e2ae1beb",
+    "url": "/static/media/Star.f965963f.png"
+  },
+  {
+    "revision": "17b46423365ee8c6ee4bcc15eb1aa06d",
+    "url": "/static/media/Video.17b46423.m4v"
+  },
+  {
+    "revision": "cd48ef73d0e2d80b0c79f4e40b666fd6",
+    "url": "/static/media/bg4.cd48ef73.jpg"
+  }
+]);
